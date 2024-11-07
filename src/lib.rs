60330a3e824251c0c7745a0f5a46738645638358
Dominik Werder <dominik.werder@gmail.com>
2024-11-07 20:57:38 +0100

    Factored into separate crate

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+pub mod collect_s;
+pub mod container;
+pub mod events;
+pub mod framable;
+pub mod isodate;
+pub mod overlap;
+pub mod scalar_ops;
+pub mod streamitem;
+pub mod subfr;
+pub mod test;
+pub mod timebin;
+pub mod transform;
+pub mod vecpreview;
+
+pub mod bincode {
+    pub use bincode::*;
+}
+
+pub use events::*;
