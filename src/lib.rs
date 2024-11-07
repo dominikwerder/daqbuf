34324db9764d4ee3412a57ac969d7cb8e814cc4c
Dominik Werder <dominik.werder@gmail.com>
2024-11-07 21:21:58 +0100

    Factored into separate crate

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+pub mod series;
+
+pub use series::ChannelStatusSeriesId;
+pub use series::SeriesId;
+
+// use log::*;
+
+// pub fn log_test() {
+//     info!("log-test");
+//     warn!("log-test");
+//     error!("log-test");
+//     debug!("log-test");
+//     trace!("log-test");
+// }
