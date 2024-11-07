29bee7c9e4ff6e454a5b13944d1194a1cd2dab41
Dominik Werder <dominik.werder@gmail.com>
2024-11-07 21:11:24 +0100

    Factored into separate crate

---
+++ src/binning.rs
@@ -0,0 +1,11 @@
+pub mod aggregator;
+pub mod binnedvaluetype;
+pub mod container_bins;
+pub mod container_events;
+pub mod timeweight;
+pub mod valuetype;
+
+#[cfg(test)]
+mod test;
+
+use super::binning as ___;
